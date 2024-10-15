7eda3248f646e6f9eb171563ffd954c82b2327ad
feranmi <feranmygit@gmail.com>
2024-10-15 05:44:52 -0700

    second commit

---
+++ com-dev/src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import TheHome from './components/TheHome.vue'
+</script>
+
+<template>
+  <div class="welcome">
+  
+
+    <RouterView />
+  </div>
+
+</template>
+
+<style scoped>
+
+</style>
